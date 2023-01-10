5dd0d7f7baaad9e6e7165d5c81595201d395b31a
alexcloudstar <alexcloudstar@gmail.com>
2023-01-10 16:50:46 +0200

    Fix build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,15 +9,18 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-  ],
+    ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
   },
+  features: {
+    storyStoreV7: true,
+  },
   async viteFinal(config) {
     const { config: userConfig } = await loadConfigFromFile(
-      path.resolve(__dirname, '../vite.config.ts')
-    )
+            path.resolve(__dirname, '../vite.config.ts')
+            )
 
     return mergeConfig(config, {
       ...userConfig,
