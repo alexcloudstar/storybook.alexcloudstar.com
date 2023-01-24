c975f01f607720e78bf6e135c0069278dbaea046
alexcloudstar <alexcloudstar@gmail.com>
2023-01-24 22:33:13 +0200

    Add defaultValues const

--- src/stories/Footer/Footer.stories.tsx
+++ src/stories/Footer/Footer.stories.tsx
@@ -2,30 +2,31 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import Footer from './Footer'
 
-const defaultLinks = [
-  {
-    url: 'https://google.com',
-    text: 'Home',
-  },
-  {
-    url: 'https://google.com',
-    text: 'About',
-  },
-  {
-    url: 'https://google.com',
-    text: 'Technologies',
-  },
-  {
-    url: 'https://google.com',
-    text: 'Projects',
-  },
-  {
-    url: 'https://google.com',
-    text: 'Contact',
-  },
-]
-
-const defaultCopyright = '© 2021 - All Rights Reserved'
+const defaultValues = {
+  links: [
+    {
+      url: 'https://google.com',
+      text: 'Home',
+    },
+    {
+      url: 'https://google.com',
+      text: 'About',
+    },
+    {
+      url: 'https://google.com',
+      text: 'Technologies',
+    },
+    {
+      url: 'https://google.com',
+      text: 'Projects',
+    },
+    {
+      url: 'https://google.com',
+      text: 'Contact',
+    },
+  ],
+  copyright: '© 2021 - All Rights Reserved',
+}
 
 export default {
   title: 'Components/Footer',
@@ -35,13 +36,13 @@ export default {
       control: {
         type: 'object',
       },
-      defaultValue: defaultLinks,
+      defaultValue: defaultValues.links,
     },
     copyright: {
       control: {
         type: 'text',
       },
-      defaultValue: defaultCopyright,
+      defaultValue: defaultValues.copyright,
     },
   },
 } as ComponentMeta<typeof Footer>
@@ -50,7 +51,4 @@ const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />
 
 export const Basic = Template.bind({})
 
-Basic.args = {
-  links: defaultLinks,
-  copyright: defaultCopyright,
-}
+Basic.args = defaultValues
--- src/stories/FooterSocial/FooterSocial.stories.tsx
+++ src/stories/FooterSocial/FooterSocial.stories.tsx
@@ -2,19 +2,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import FooterSocial from './FooterSocial'
 
-export default {
-  title: 'Components/FooterSocial',
-  component: FooterSocial,
-  argTypes: {},
-} as ComponentMeta<typeof FooterSocial>
-
-const Template: ComponentStory<typeof FooterSocial> = (args) => (
-  <FooterSocial {...args} />
-)
-
-export const Basic = Template.bind({})
-
-Basic.args = {
+const defaultValues = {
   logo: 'https://scontent.fias1-1.fna.fbcdn.net/v/t39.30808-6/316261542_576485601146391_3311618198461853349_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rAGb9rNDIvYAX8i4fdg&_nc_ht=scontent.fias1-1.fna&oh=00_AfC3cHdWFXwrApHm_5k9RvplBHXmTAkgcrSJ6BGmwv6Z3g&oe=63D4933D',
   phone: '1234567890',
   email: 'test@test.com',
@@ -33,3 +21,42 @@ Basic.args = {
     },
   ],
 }
+
+export default {
+  title: 'Components/FooterSocial',
+  component: FooterSocial,
+  argTypes: {
+    logo: {
+      control: {
+        type: 'text',
+        default: defaultValues.logo,
+      },
+    },
+    phone: {
+      control: {
+        type: 'text',
+        default: defaultValues.phone,
+      },
+    },
+    email: {
+      control: {
+        type: 'text',
+        default: defaultValues.email,
+      },
+    },
+    social: {
+      control: {
+        type: 'object',
+        default: defaultValues.social,
+      },
+    },
+  },
+} as ComponentMeta<typeof FooterSocial>
+
+const Template: ComponentStory<typeof FooterSocial> = (args) => (
+  <FooterSocial {...args} />
+)
+
+export const Basic = Template.bind({})
+
+Basic.args = defaultValues
